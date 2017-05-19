e64c703cf7fb99d405175e7696b78320df4d8627
marcelo.fontes <marcelo.fontes@bemobi.com.br>
2017-05-19 11:50:06 -0300

    first commit

---
+++ src/main/java/com/test/sedaqueue/SedaQueueApplication.java
@@ -0,0 +1,38 @@
+package com.test.sedaqueue;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class SedaQueueApplication implements CommandLineRunner {
+
+	@Autowired
+	@EndpointInject(uri = "direct://direct-queue")
+	ProducerTemplate producerTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedaQueueApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		producerTemplate.sendBody("Teste Direct - Async");
+	}
+
+	@Component
+	class Router extends RouteBuilder {
+
+		@Override
+		public void configure() throws Exception {
+			from("direct://direct-queue").routeId("toAsync").to("seda://async-queue");
+
+			from("seda://async-queue?size=100").routeId("toLog").log("${body}");
+		}
+	}
+}
---
+++ src/test/java/com/test/sedaqueue/SedaQueueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.sedaqueue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SedaQueueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
